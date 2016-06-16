54923ea0c81c6616e012a2e03c4a64b4f553d64f
Milos Bejda <mbejda@live.com>
2016-06-16 07:08:37 -0400

    Fixed grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -338,7 +338,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'ngdeployFrontendApp',
+          module: 'ngdeployApp',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
