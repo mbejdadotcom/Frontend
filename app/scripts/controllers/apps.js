a9946b1011e44f6b14587ee355dc2e29172f257d
Milos Bejda <mbejda@live.com>
2016-06-26 04:08:28 -0400

    added id

--- app/scripts/controllers/apps.js
+++ app/scripts/controllers/apps.js
@@ -8,58 +8,55 @@
  * Controller of the ngdeployApp
  */
 angular.module('ngdeployApp')
-  .controller('AppsCtrl', function($scope, appService, token, userService, $uibModal, $log, sweet, teams,git) {
-    userService.self().then(function(user){
-      console.log("ME ",user)
-    })
+  .controller('AppsCtrl', function ($rootScope,$scope, appService, token, userService, $uibModal, $log, sweet, teams,dbUser,git) {
+
+    $scope.user = dbUser;
+
+
     $scope.token = token;
-    $scope.loadApps = function() {
-      appService.get().then(function(response) {
+    $scope.loadApps = function () {
+      appService.get().then(function (response) {
         $scope.apps = response;
       });
-    }
+    };
 
-    $scope.createApplication = function(ngDeployUrl, name) {
+
+
+    $scope.createApplication = function (ngDeployUrl, name) {
       appService.post({
         ngDeployUrl: ngDeployUrl,
         name: name
-      }).then(function(response) {
+      }).then(function (response) {
 
-        if($scope.link_repo!= -1 && typeof response.id != 'undefined'){
 
-          git.hookIt(response.id, $scope.link_repo).then(function() {
-            sweet.show('Git Repo Hooked!', 'We hooked the repo successfully and the next push will be deployed automatically.', 'success');
-          }, function(error) {
-            sweet.show('Error', error.error, 'error');
-          })
-
-        }
-        else{
-          sweet.show('Created!', 'The application has been created.', 'success');
-        }
+        sweet.show('Created!', 'The application has been created.', 'success');
 
         $scope.loadApps();
+      }, function (error) {
+        sweet.show('Oh no!', error, 'error');
       })
-    }
+    };
 
-    $scope.repositories = []
-    $scope.link_repo = -1;
-
-    $scope.purgeCache = function(app, stage) {
-      console.log(app)
+    $scope.purgeCache = function (app, stage) {
       appService.purge({
         ngDeployUrl: app.ngDeployUrl,
-      }).then(function() {
+      }).then(function () {
         sweet.show('Cache Purged!', 'Application cache has been purged', 'success');
 
-      }, function(error) {
+      }, function (error) {
         sweet.show('Error', error.error, 'error');
 
-      })
+      });
 
 
+    };
+    $scope.cli = function (name) {
+      return 'ngdeploy init '+name+' .';
     }
-    $scope.promote = function(app, stage) {
+
+
+
+    $scope.promote = function (app, stage) {
       sweet.show({
         title: 'Confirm',
         text: 'Promote to ' + stage + '?',
@@ -68,12 +65,12 @@ angular.module('ngdeployApp')
         confirmButtonText: 'Yes, promote it!',
         closeOnConfirm: false,
         closeOnCancel: false
-      }, function(isConfirm) {
+      }, function (isConfirm) {
         if (isConfirm) {
           appService.promote({
-            ngDeployUrl: app.ngDeployUrl,
+            id: app.id,
             stage: stage
-          }).then(function() {
+          }).then(function () {
             sweet.show('Promoted!', 'Application has been promoted to ' + stage, 'success');
 
           })
@@ -82,9 +79,9 @@ angular.module('ngdeployApp')
           sweet.show('Cancelled', 'Nothing changed', 'error');
         }
       });
-    }
+    };
 
-    $scope.delete = function(ngDeployUrl, team, apps) {
+    $scope.delete = function (ngDeployUrl, team, apps) {
       if (team.type == "owner") {
 
         sweet.show({
@@ -95,12 +92,12 @@ angular.module('ngdeployApp')
           confirmButtonText: 'Yes, delete it!',
           closeOnConfirm: false,
           closeOnCancel: false
-        }, function(isConfirm) {
+        }, function (isConfirm) {
           if (isConfirm) {
-            appService.delete(ngDeployUrl).then(function(response) {
+            appService.delete(ngDeployUrl).then(function (response) {
               sweet.show('Deleted!', 'The application has been deleted.', 'success');
               $scope.loadApps();
-            }, function(error) {
+            }, function (error) {
 
               console.log(error)
             });
@@ -110,10 +107,6 @@ angular.module('ngdeployApp')
           }
         });
       }
-
-
-
-
 
 
       if (team.type == "user") {
@@ -127,13 +120,13 @@ angular.module('ngdeployApp')
           confirmButtonText: 'Yes, remove me!',
           closeOnConfirm: false,
           closeOnCancel: false
-        }, function(isConfirm) {
+        }, function (isConfirm) {
           if (isConfirm) {
 
             teams.delete({
               appId: team.appId,
               userId: team.userId
-            }).then(function(results) {
+            }).then(function (results) {
               sweet.show('Remove!', 'You have been removed as a collaborator', 'success');
 
 
@@ -148,61 +141,53 @@ angular.module('ngdeployApp')
       }
 
 
-    }
-    $scope.setupHosting = function(app) {
+    };
+    $scope.setupHosting = function (app) {
       var modalInstance = $uibModal.open({
         animation: true,
         templateUrl: 'setupHosting.html',
         controller: 'setupHostingCtrl',
         size: 'lg',
         resolve: {
-          app: function() {
+          app: function () {
             return app
           }
         }
       });
 
-      modalInstance.result.then(function(selectedItem) {
+      modalInstance.result.then(function (selectedItem) {
         $scope.selected = selectedItem;
-      }, function() {
+      }, function () {
         $log.info('Modal dismissed at: ' + new Date());
       });
-    }
+    };
 
-    $scope.upgrade = function(app) {
+    $scope.upgrade = function (app) {
 
       sweet.show({
         title: 'Confirm',
         text: 'Upgrade to production hosting?',
         showCancelButton: true,
         confirmButtonText: 'Yes, upgrade it!',
-        closeOnConfirm: false,
-        closeOnCancel: false
-      }, function(isConfirm) {
+        closeOnConfirm: true,
+        closeOnCancel: true
+      }, function (isConfirm) {
         if (isConfirm) {
-          appService.upgrade(app.ngDeployUrl).then(function(response) {
+          appService.upgrade(app.id).then(function (response) {
             sweet.show('Deleted!', 'The application has been deleted.', 'success');
             $scope.loadApps();
-          }, function(error) {
+          }, function (error) {
+            sweet.show('Upgrade Failed !', error, 'error');
 
-            console.log(error)
           });
 
         } else {
           sweet.show('Cancelled', 'Your imaginary file is safe :)', 'error');
         }
       });
-    }
 
-    $scope.listRepos= function listRepos(){
-      git.listRepos().then(function(repos){
-        repos.forEach(function(item){
-          if(item.permissions.admin){
-            $scope.repositories.push(item);
-          }})
-      })
-    }
+
+    };
 
     $scope.loadApps();
-    $scope.listRepos();
   });
